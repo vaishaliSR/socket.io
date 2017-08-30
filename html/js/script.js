7d3794d9de1c5a55466eb31ff290a23e453a53c7
Kunika Rathore <Kunika.Rathore@prodigi.local>
2017-08-30 17:33:44 +0530

    added id - index.js

--- html/js/script.js
+++ html/js/script.js
@@ -1,29 +1,25 @@
 $(document).ready(function() {
-
-
-	 $(".section1").hide();
+$(".section1").hide();
 $(".navigation div").click(function () {
     $(".navigation div").removeClass("border-bottom");
-    // $(".tab").addClass("active"); // instead of this do the below 
     $(this).toggleClass("border-bottom");   
 });
 $(".navigation div").click(function () {
-	//$(".section1").show();
+
+	// for user profile section
 	if(this.className === "users center border-bottom"){
      $(".section1").show();
      $(".section2").hide();
      $("body").addClass("bg_none");
 
 	}
+
+	// for chat box section
 	else if(this.className === "chats center border-bottom"){
      $(".section1").hide();
      $(".section2").show();
      $("body").removeClass("bg_none");
 	}
-    //$(".section1").toggle();
-    // $(".tab").addClass("active"); // instead of this do the below 
-   // $(".section2").toggle();   
-    
 });
 });
 
